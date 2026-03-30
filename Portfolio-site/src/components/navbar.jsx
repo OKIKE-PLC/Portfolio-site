8780bc185afbda5bc18534860c038edded894441
Ino Tech <ezekieldavid150@gmail.com>
2026-03-30 14:58:56 +0100

    feat: update Navbar to use React Router Links and enhance Contact page with form and contact details

--- Portfolio-site/src/components/navbar.jsx
+++ Portfolio-site/src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import { RiMenu4Line } from "react-icons/ri";
+import { Link } from "react-router-dom";
 import darkLogo from '../assets/Dark.png';
 
 export default function Navbar() {
@@ -16,14 +17,14 @@ const toggleMobileMenu = () => {
         
 
         <section className='flex justify-between items-center' style={{padding: "1rem"}}>
-        <a href="#"><img src={darkLogo} alt="Okike Logo" width={60} /></a>
+        <Link to="/"><img src={darkLogo} alt="Okike Logo" width={60} /></Link>
             <nav className='hidden md:flex justify-center items-center gap-5 text-sm '>
-                <a href="#" className='hover:text-orange-600 hover:underline transition duration-150'>Home</a>
-                <a href="#" className='hover:text-orange-600 hover:underline transition duration-150'>Portfolio</a>
-                <a href="#" className='hover:text-orange-600 hover:underline transition duration-150'>Contact</a>
+                <Link to="/" className='hover:text-orange-600 hover:underline transition duration-150'>Home</Link>
+                <Link to="/portfolio" className='hover:text-orange-600 hover:underline transition duration-150'>Portfolio</Link>
+                <Link to="/contact" className='hover:text-orange-600 hover:underline transition duration-150'>Contact</Link>
             </nav>
 
-            <a href="#" className='hidden md:block bg-orange-600 text-stone-100 rounded-xl hover:bg-orange-800 transition duration-300' style={{padding: ".3rem .5rem"}}>Contact Us</a>
+            <Link to="/contact" className='hidden md:block bg-orange-600 text-stone-100 rounded-xl hover:bg-orange-800 transition duration-300' style={{padding: ".3rem .5rem"}}>Contact Us</Link>
 
             {/* mobile navigation visible only on mobile */}
             <button onClick={toggleMobileMenu} className='md:hidden text-3xl p-2 z-50'>
@@ -31,9 +32,9 @@ const toggleMobileMenu = () => {
             </button>
             <div id='mobileMenu' className='mob  hidden fixed top-11.5 md:hidden bg-opacity-70 backdrop-blur-md items-start'>
                 <nav className='flex flex-col gap-6 items-center'>
-                    <a href="#" className='hover:text-orange-600 hover:underline transition duration-150'>Home</a>
-                <a href="#" className='hover:text-orange-600 hover:underline transition duration-150'>Portfolio</a>
-                <a href="#" className='hover:text-orange-600 hover:underline transition duration-150'>Contact</a>
+                    <Link to="/" className='hover:text-orange-600 hover:underline transition duration-150'>Home</Link>
+                <Link to="/portfolio" className='hover:text-orange-600 hover:underline transition duration-150'>Portfolio</Link>
+                <Link to="/contact" className='hover:text-orange-600 hover:underline transition duration-150'>Contact</Link>
                 </nav>
             </div>
         </section>
--- Portfolio-site/src/pages/contact.jsx
+++ Portfolio-site/src/pages/contact.jsx
@@ -1,7 +1,116 @@
+import { CiLocationOn } from "react-icons/ci";
+import { MdOutlineEmail } from "react-icons/md";
+import { useState } from "react";
+
 export default function Contact() {
+    const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    service: "",
+    budget: "",
+    timeline: "",
+    message: "",
+    });
+
+    const handleChange = (e) => {
+    setFormData((prev) => ({
+    ...prev,
+    [e.target.name]: e.target.value,
+    }));
+};
+
+
     return (
-        <div>
-            <h1>Contact me</h1>
-        </div>
+        <section className="mx-auto" style={{padding: "3rem .6rem"}}>
+            {/* main contact div */}
+            <div className="flex flex-col md:flex-row justify-center items-start gap-5" >
+                {/* left div */}
+                <div className="flex flex-col justify-center items-start gap-5 text-left md:w-1/2">
+                    <h1 className="text-orange-600 font-light text-xl lg:text-2xl">Get in Touch</h1>
+                    <p className="font-light">Let's discuss your next architectural landmark. Our fabrication experts and designers are ready to translate your vision into a structural monolith.</p>
+                    <div className="flex flex-col md:flex-row justify-center items-start gap-4">
+                        <div className="flex flex-col justify-center items-start gap-2">
+                            <CiLocationOn className="text-orange-600 font-bold text-3xl"/>
+                            <div className="flex flex-col justify-center items-start gap-2">
+                                <h3>Headquarters</h3>
+                                <p>Industrial Zone 4, Architectural District Lagos, Nigeria</p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col justify-center items-start gap-2">
+                            <MdOutlineEmail className="text-orange-600 font-bold text-3xl"/>
+                            <div className="flex flex-col justify-center items-start gap-2">
+                                <h3>Inquiries</h3>
+                                <a href="mailto:info@okikeplc.com">info@okikeplc.com</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* right/contact div */}
+                <div className="flex flex-col justify-center items-start gap-5 text-left  w-full md:w-1/2 rounded-xl" >
+                    <h3 className="text-orange-600 font-light text-xl lg:text-2xl">Project Brief</h3>
+                    <form className="space-y-5 flex flex-col justify-center w-full items-start gap-3">
+                    <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        placeholder="Full name"
+                        className="w-full border border-stone-300 rounded-xl px-4 py-3 outline-none focus:border-orange-600"
+                        required
+                        
+
+                        style={{padding: ".5rem"}}
+                    />
+
+                    <input
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        placeholder="Business email"
+                        className="w-full border border-stone-300 rounded-xl px-4 py-3 outline-none focus:border-orange-600"
+                        required
+
+                        style={{padding: ".5rem"}}
+                    />
+
+                    <select
+                        name="service"
+                        value={formData.service}
+                        onChange={handleChange}
+                        className="w-full border border-stone-300 rounded-xl px-4 py-3 outline-none focus:border-orange-600"
+                        required
+                        style={{padding: ".5rem"}}
+                        >
+                        <option value="">Select service</option>
+                        <option>Interior Design</option>
+                        <option>3D and Architectural Design</option>
+                        <option>Residential</option>
+                    </select>
+
+                    <textarea
+                        rows="5"
+                        name="message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        placeholder="Tell us about your project goals..."
+                        className="w-full border border-stone-300 rounded-xl px-4 py-3 outline-none focus:border-orange-600"
+                    required
+                    style={{padding: ".5rem"}}
+                    />
+
+                    <button
+                            type="submit"
+                            // disabled={loading}
+                            className="w-full bg-orange-600 text-white  py-4 rounded-xl hover:bg-orange-700 transition font-medium" 
+                            style={{padding: ".5rem"}}
+                    >
+                        Submit
+                    </button>
+
+                    </form>
+                    </div>
+            </div>
+        </section>
     )
 }
